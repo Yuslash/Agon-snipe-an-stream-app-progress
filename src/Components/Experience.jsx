1deeee5f889ae492fc07bd78f42d2b01429e5146
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-06 17:44:12 +0530

    No more z-index fight now

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import StartingPage from "./StartingPage";
-
 
 export default function Exprerience()
 {
-    return <div className="absolute top-0 left-0 w-full h-full bg-black text-white">
-        <StartingPage />
+    const [initialAudio, setInitialAudio ] = useState(false)
+    let audio
+
+    useEffect(() => {
+
+        audio = new Audio('/HoverSounds/spiderman.mp3')
+        
+        audio.preload = 'auto'
+
+        audio.loop = true
+    }, [])
+
+    const enableAudio = () =>
+    {
+        setInitialAudio(true)
+
+        audio.play()
+    }
+
+    return <div className="absolute top-0 left-0 w-full h-full bg-black text-white overflow-hidden">
+        {!initialAudio && (
+            <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black text-white overflow-hidden">
+                <button onClick={enableAudio} className=" p-8 font-semibold border border-white rounded-full text-2xl hover:rounded">Enable Audio</button>
+            </div>
+        )}
+        
+        {initialAudio && <StartingPage />}
+       
     </div>
 }
--- src/Components/StartingPage.jsx
+++ src/Components/StartingPage.jsx
@@ -1,13 +1,35 @@
 export default function StartingPage()
 {
+
     return <>
-        <div className="mass w-full h-full absolute top-0 left-0 overflow-hidden">
+        <div className="mass w-full h-full absolute top-0 left-0 bg-black object-cover bg-center bg-no-repeat overflow-hidden">
             <video
             autoPlay 
             className="ivanthanda"
             src="ivanthandamassuh.webm"
-            loop   
+            loop
+            muted   
             />
+        </div>
+        <div className="gradient-over flex absolute top-0 left-0 w-full h-full ">
+            <div className="top-nav absolute top-0 left-0 w-full "></div>
+                <div className="left-row w-full h-full justify-center flex flex-col ">
+                
+                    <div className="flex flex-col mt-20 gap-5 px-[180px] ">
+                        <div className=" w-full flex px-28 ">
+                            <span className="over">overwatch</span>
+                        </div>
+                        <span className="the-hunt">THE HUNT <br /> IS AWAIT</span>
+                        <span className="kaka">Some Description about something that is
+                        not look correct so be care full</span>
+                        <div className=" flex px-20 mt-6 w-full">
+                            <img className="fortnite-button" src="/Buttons/fortnite.png"></img>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="right-row w-full h-full"></div>
         </div>
     </>
 }
