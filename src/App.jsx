c12c5f9e9b4f533dc2ce98079371820862b2a035
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-06 14:38:33 +0530

    removed the imports that removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AnimateBox from './Animations/AnimateBox.jsx'
 import ScaleUpAnimation from "./Animations/ScaleUpAnimation.jsx"
-import ParallaxImage from "./Parallax/ParallaxImage.jsx"
-import ParallaxVideo from "./Parallax/ParallaxVideo.jsx"
-import GetStarted from "./App/GetStarted.jsx"
 import Experience from "./Components/Experience.jsx"
 
 export default function App()
@@ -13,11 +10,6 @@ export default function App()
         <Routes>
           <Route path="/gsap" element={<AnimateBox />} />
           <Route path="/scaleup" element={<ScaleUpAnimation />} />
-          <Route path="/parallax" element={<ParallaxImage />}/>
-          <Route path="/paraVideo" element={<ParallaxVideo />} />
-
-          {/* GET STARTED */}
-          <Route path="/start" element={<GetStarted />} />
           <Route path="/init" element={<Experience />}/>
         </Routes>
       </BrowserRouter>
