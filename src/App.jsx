713e4e4e396410e7b539d563021eccc2dd8ecb54
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-06 14:40:17 +0530

    removed Parallax cursor

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,6 @@ export default function App()
           <Route path="/scaleup" element={<ScaleUpAnimation />} />
           <Route path="/init" element={<Experience />}/>
         </Routes>
-      </BrowserRouter>
-
-      
+      </BrowserRouter>      
   </>
 }
