b3fc466fc7014883aaed171c5bc649097e3cf933
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-06 14:41:54 +0530

    Thats it got the another issue

--- src/Components/StartingPage.jsx
+++ src/Components/StartingPage.jsx
@@ -1,7 +1,7 @@
 export default function StartingPage()
 {
     return <>
-        <div className="mass w-full h-full absolute top-0 left-0 bg-purple-500 overflow-hidden">
+        <div className="mass w-full h-full absolute top-0 left-0 overflow-hidden">
             <video
             autoPlay 
             className="ivanthanda"
