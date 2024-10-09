8103d31ed8b7db781c553314e2d82967a7d462db
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-10 00:34:45 +0530

    completed very hard diffulty buts its only front-end next connect the backend

--- src/Uploads/Upload.jsx
+++ src/Uploads/Upload.jsx
@@ -1,8 +1,6 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import './IconUpload.css'
-import Site from "./Site"
-import Bubbles from "./Bubbles"
 
 export default function Upload() {
 
@@ -25,10 +23,55 @@ export default function Upload() {
 
     }, [navigate])
 
-    return <div className="main-upload absolute top-0 left-0 w-full h-full text-white px-96">
-        <div className="upload-panel flex">
-            <div className="w-full"></div>
-            <div className="w-[560px]"></div>
+    return <div className="main-upload absolute flex justify-center items-center top-0 left-0 w-full h-full text-white px-[120px] py-[40px]">
+        <div className="upload-panel p-5 w-full h-full flex">
+            <div className="w-full flex flex-col gap-2 py-10 px-16 ">
+                <span className=" text-3xl font-semibold">Let's Upload Your Monster Content! ✨</span>
+                <span className=" text-sm font-normal">All fields are Required and Select both Images and Video to Upload</span>
+
+                <div className="all-input-field flex flex-col gap-4 mt-4">
+                    <span className="text-xl tracking-wide text-gray-300 font-semibold">Enter Title For The Video</span>
+                    <input
+                     
+                     className="super-input-field w-full"    
+                    placeholder="Title"
+
+                    />
+
+                    <span className="text-xl tracking-wide text-gray-300 font-semibold">Enter Description For The Video</span>
+                    <textarea
+                     
+                     className="super-input-field h-[120px] w-full"    
+                    placeholder="Description"
+
+                    />
+                    
+                    <h1 className="text-xl tracking-wide">Select Game</h1>
+                    <select className="super-input-field">
+                        <option></option>
+                    </select>
+
+                    <div className="flex gap-4 w-full h-full">
+                        {/* Blue Image with File Upload */}
+                        <label className="flex-1 cursor-pointer">
+                            <img className="w-full h-full object-contain" src="/upload/uploadblue.png" alt="Upload Blue" />
+                            <input type="file" className="hidden" accept="video/*" />
+                        </label>
+
+                        {/* Purple Image with File Upload */}
+                        <label className="flex-1 cursor-pointer">
+                            <img className="w-full h-full object-contain" src="/upload/uploadpurple.png" alt="Upload Purple" />
+                            <input type="file" className="hidden" accept="image/*" />
+                        </label>
+                    </div>
+
+                    <button className="discord-button py-4 rounded-lg font-semibold text-xl">Upload</button>
+
+
+                </div>
+
+            </div>
+            <img src="/some.png" className="w-[660px] rounded-2xl"></img>
         </div>
     </div>
 }
