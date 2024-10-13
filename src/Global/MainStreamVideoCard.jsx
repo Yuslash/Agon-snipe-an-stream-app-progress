1e892fb0a1a33b1de4e17d26e1afd7c1449b8338
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-13 20:30:36 +0530

    Brain is not Braining

--- src/Global/MainStreamVideoCard.jsx
+++ src/Global/MainStreamVideoCard.jsx
@@ -1,9 +1,12 @@
 export default function MainStreamVideoCard() {
     
     return <>
-        <div className='p-3 w-full h-full bg-orange-300'>
-            <div className='main-video-card-top w-full bg-purple-400 p-3 flex'>
-
+        <div className='p-3 w-full h-full '>
+            <div className='main-video-card-top w-full py-5 flex gap-9'>
+                <button className="hover:text-purple-500 hover:underline">Top Streamer</button>
+                <button className="hover:text-purple-500 hover:underline">Hot Streamer</button>
+                <button className="hover:text-purple-500 hover:underline">New Streamer</button>
+                <button className="hover:text-purple-500 hover:underline">Top Clip</button>
             </div>
         </div>
     </>
