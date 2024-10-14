f545825e42fffdc28ab16286a08d8597d834c682
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-14 20:08:00 +0530

    Gettting Bored

--- src/Auth/AstroLoginPage.jsx
+++ src/Auth/AstroLoginPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import './Auth.css'
+import './Astro.css'
 import { useEffect, useRef, useState } from 'react'
 import { toast, ToastContainer, Bounce } from 'react-toastify'
 
@@ -139,9 +140,9 @@ export default function AstroLoginPage() {
         }
     }
 
-    return <div ref={imageRef} className="login-image w-full h-full absolute top-0 left-0 bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center" style={{ backgroundImage: "url('/authentication/background/5.png')" }}>
+    return <div ref={imageRef} className="login-image w-full h-full absolute top-0 left-0 bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center" style={{ backgroundImage: "url('/authentication/background/3.jpg')" }}>
         <ToastContainer />
-        <div className='top-navbar py-3 px-8 flex justify-between absolute top-0 left-0 w-full'>
+        <div className='astro-top-navbar py-3 px-8 flex justify-between absolute top-0 left-0 w-full'>
             <div className='flex items-center gap-4'>
                 <img src='/authentication/logo.svg' />
                 <span className='agon-head'>AGON SNIPE</span>
@@ -149,17 +150,17 @@ export default function AstroLoginPage() {
 
             <button onClick={() => { home(), wooshSound() }} className='discord-button flex gap-2 px-5 items-center'><img src='/authentication/home.png' /><span>GO BACK</span></button>
         </div>
-        <div className="hunt-login-panel mt-[120px] px-9 relative flex flex-col justify-center items-center w-[430px]">
+        <div className="astro-login-panel mt-[120px] px-9 relative flex flex-col justify-center items-center w-[430px]">
             <img className='relative top-[-50px]' src='/authentication/profile.png' />
             <span className='superwelcome-text'>WELCOME BACK</span>
-            <div className='main-buttons-hold flex w-full p-1 mt-7 tracking-wider rounded-2xl justify-center'>
-                <button onClick={moveToReg} className='regiser-button w-full rounded-2xl'>REGISTER</button>
-                <button className='login-button w-full rounded-2xl'>LOGIN</button>
+            <div className='astro-main-buttons-hold flex w-full p-1 mt-7 tracking-wider rounded-2xl justify-center'>
+                <button onClick={moveToReg} className='astro-regiser-button w-full rounded-2xl'>REGISTER</button>
+                <button className='astro-login-button w-full rounded-2xl'>LOGIN</button>
             </div>
             <div className=' w-full flex gap-1 flex-col mt-4'>
-                <span className='above-text'>Username</span>
+                <span className='astro-above-text'>Username</span>
                 <input
-                    className='text-input p-4'
+                    className='astro-text-input p-4'
                     placeholder='Enter Your Username'
                     type='text'
                     value={username}
@@ -168,9 +169,9 @@ export default function AstroLoginPage() {
             </div>
 
             <div className=' w-full flex gap-1 flex-col mt-4'>
-                <span className='above-text'>Password</span>
+                <span className='astro-above-text'>Password</span>
                 <input
-                    className='text-input p-4'
+                    className='astro-text-input p-4'
                     placeholder='Enter Your Password'
                     type='password'
                     value={password}
@@ -179,7 +180,7 @@ export default function AstroLoginPage() {
             </div>
 
             <div className='flex flex-col gap-5 w-full justify-center mt-[60px]'>
-                <button onClick={checkUserData} className='to-account w-full py-4'><span>LOGIN TO ACCOUNT</span></button>
+                <button onClick={checkUserData} className='astro-to-account w-full py-4'><span>LOGIN TO ACCOUNT</span></button>
                 <img className='' src='/authentication/orline.png'></img>
                 <button className='discord-button mb-[30px] py-[15px] flex justify-center items-center gap-[10px]'><img src='/authentication/discord.png'></img><span>LOGIN WITH DISCORD</span></button>
             </div>
